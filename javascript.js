c17002b075c874a629dd0ba67d83aa4d79d63fd6
Vamsi Krishna Thigulla <43385381+VamsiKrishnaThigulla@users.noreply.github.com>
2020-06-22 01:09:18 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,55 @@
+const body = document.body;
+const menuLinks = document.querySelectorAll(".admin-menu a");
+const collapseBtn = document.querySelector(".admin-menu .collapse-btn");
+const toggleMobileMenu = document.querySelector(".toggle-mob-menu");
+const collapsedClass = "collapsed";
+
+collapseBtn.addEventListener("click", function() {
+  this.getAttribute("aria-expanded") == "true"
+    ? this.setAttribute("aria-expanded", "false")
+    : this.setAttribute("aria-expanded", "true");
+  this.getAttribute("aria-label") == "collapse menu"
+    ? this.setAttribute("aria-label", "expand menu")
+    : this.setAttribute("aria-label", "collapse menu");
+  body.classList.toggle(collapsedClass);
+});
+
+toggleMobileMenu.addEventListener("click", function() {
+  this.getAttribute("aria-expanded") == "true"
+    ? this.setAttribute("aria-expanded", "false")
+    : this.setAttribute("aria-expanded", "true");
+  this.getAttribute("aria-label") == "open menu"
+    ? this.setAttribute("aria-label", "close menu")
+    : this.setAttribute("aria-label", "open menu");
+  body.classList.toggle("mob-menu-opened");
+});
+
+for (const link of menuLinks) {
+  link.addEventListener("mouseenter", function() {
+    body.classList.contains(collapsedClass) &&
+    window.matchMedia("(min-width: 768px)").matches
+      ? this.setAttribute("title", this.textContent)
+      : this.removeAttribute("title");
+  });
+}
+
+function clearListCookies()
+{   
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++)
+    {   
+        var spcook =  cookies[i].split("=");
+        deleteCookie(spcook[0]);
+    }
+    function deleteCookie(cookiename)
+    {
+        var d = new Date();
+        d.setDate(d.getDate() - 1);
+        var expires = ";expires="+d;
+        var name=cookiename;
+        //alert(name);
+        var value="";
+        document.cookie = name + "=" + value + expires + "; path=/acc/html";                    
+    }
+    window.location = ""; // TO REFRESH THE PAGE
+}
